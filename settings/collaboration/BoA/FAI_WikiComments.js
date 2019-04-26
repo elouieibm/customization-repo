08c636db8f071565c7e58a8f0b5302d175ba5379
Eugene Louie <eugene_louie@us.ibm.com>
2019-04-25 23:51:50 -0400

    Switch to use a REST call instead of screen scrape

--- settings/collaboration/BoA/FAI_WikiComments.js
+++ settings/collaboration/BoA/FAI_WikiComments.js
@@ -64,131 +64,12 @@
             } else return null;
         };
 
-       function customFlagPopup (comment){
-		var ds = this._moreDs;
-
-		var custFlagEntryCommentDiv = document.createElement("div");
-		custFlagEntryCommentDiv.id = "custFlagEntryCommentDiv";
-		custFlagEntryCommentDiv.style.cssText='background-color:#ffffff';
-		custFlagEntryCommentDiv.innerHTML="<div class='lotusDialogHeader' style='background-color: #F2F2F2;background-image: none;border: medium none #CCCCCC;border-radius: 0 0 0 0;box-shadow: none;color: #444444;font-size: 1.2em;line-height: 1.2;margin: 0;padding: 8px 0 8px 10px;text-align: center;'><h1 class='lotusHeading' style='color: #444444;font-size: 1.2em;line-height: 1.2;text-align: left;'>Flag as inappropriate</h1></div><div class='lotusDialogContent' style='max-height: 200px;overflow: auto;padding: 5px 10px;'><div class='lotusFormField'><label for='cust_input_comment_for_comment'>Provide a reason for flagging this entry (optional):</label><div><textarea id='cust_input_comment_for_comment' style='width:80%' rows='7' name='comment' title='Provide a reason for flagging this entry (optional):'></textarea></div></div></div><div class='lotusDialogFooter' style='background-color: #FFFFFF;border-top: 1px solid #CCC3B2;margin: 0 10px;padding: 15px 0;text-align:right;'><input id='custFlagCommentSubmitButton' class='lotusFormButton blogsPaddingRight' type='button' value='OK' name='submitBtn' style='background-color: #0066AE;background-image: none;border: 1px solid #005097;border-radius: 0 0 0 0;box-shadow: none;color: #FFFFFF;display: inline-block;font-weight: bold;margin-right: 3px;padding: 6px 10px;text-decoration: none;text-shadow: 1px 1px 0 #1570CD;'><input id='custFlagCommentCancelButton' class='lotusFormButton blogsPaddingRight' type='button' value='Cancel' name='cancelBtn' style='background-color: #0066AE;background-image: none;border: 1px solid #005097;border-radius: 0 0 0 0;box-shadow: none;color: #FFFFFF;display: inline-block;font-weight: bold;margin-right: 3px;padding: 6px 10px;text-decoration: none;text-shadow: 1px 1px 0 #1570CD;'></div>";
-
-		var eWidgForm = new dijit.Dialog({
-			id: "eeFlagComment",
-			title: "Flag Comment",
-			content: custFlagEntryCommentDiv,
-			style: "width: 300px; height: 150px;",
-		});
-		eWidgForm.show();
-
-		var cancelButton = dojo.byId('custFlagCommentCancelButton');
-		var flagSubmitCommentButton = dojo.byId('custFlagCommentSubmitButton');
-
-		dojo.connect(cancelButton, "onclick", function() {
-				setTimeout(function() { eWidgForm.destroyRecursive(); }, 0);
-			});
-
-		var communityUuid = "";
-		var topURL = window.top.location.href;
-		var pos = topURL.indexOf("communityUuid=");
-		if (pos != -1) {
-			communityUuid = topURL.substring(pos + 14,pos + 14 + 36);
-		}
-
-		var contentAuthId = ds.getValue(comment, "author").id;
-		var statusOwner = ds.getValue(comment, "author").name;
-		var statusId = ds.getValue(comment, "id");
-		var statusContent = ds.getValue(comment, "contents");
-
-		//date fix starts here
-		var statusTimeStampOrig = ds.getValue(comment, "published");
-
-		function pad(number) {
-		if (number < 10) {
-			return '0' + number;
-		}
-		return number;
-		}
-
-		var statusTimeStamp =
-		statusTimeStampOrig.getUTCFullYear() +
-        '-' + pad(statusTimeStampOrig.getUTCMonth() + 1) +
-        '-' + pad(statusTimeStampOrig.getUTCDate()) +
-        'T' + pad(statusTimeStampOrig.getUTCHours()) +
-        ':' + pad(statusTimeStampOrig.getUTCMinutes()) +
-        ':' + pad(statusTimeStampOrig.getUTCSeconds()) +
-        '.' + (statusTimeStampOrig.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
-        'Z';
-		//date fix ends here
-		//alert(statusTimeStamp);
-
-		//var statusTimeStampOrig = ds.getValue(comment, "stateChangedDate");
-		//var statusTimeStampOrig = ds.getValue(comment, "published");
-		//alert("statusTimeStampOrig = "+statusTimeStampOrig);
-		//var statusTimeStamp = Date.parse(statusTimeStampOrig);
-		//var statusTimeStamp = statusTimeStampOrig.toISOString();
-		//var statusTimeStamp = ds.getValue(comment, "published"); - did not work
-
-		//var statusTimeStamp = ds.getPublished();  - did not work
-
-		//alert("statusTimeStamp = "+statusTimeStamp);
-		//alert("\n\nstringify ds =\n\n "+JSON.stringify(ds)+"\n\n");
-		//window.prompt("stingify ds = ",JSON.stringify(ds));
-
-		//var tempstr = JSON.stringify(ds);
-		//var pubspot = tempstr.lastIndexOf("published");
-		//var statusTimeStamp = tempstr.substring(pubspot+12,pubspot+36);
-		//alert(statusTimeStamp);
-
-
-		var flagOwnerId = this.authUser.id;
-		var flagOwner = this.authUser.name;
-
-		var allText = (dojo.query(".data")[0]).innerHTML;
-		var pos1 = allText.lastIndexOf('@all')+6;
-		var containerId = allText.substring(pos1, (allText.indexOf('\\', pos1)));
-
-
-		dojo.connect(flagSubmitCommentButton, "onclick", function() {
-			dojo.xhrPost({
-				// The URL of the request
-				url: "/BofAStatusUpdateModeration/rest/api/flagStatusUpdateComment",
-				postData: dojo.toJson({
-						communityUuid: communityUuid,
-						contentAuthorGuid: contentAuthId,
-						contentAuthorName: statusOwner,
-						contentId: statusId,
-						contentContent: statusContent,
-						contentPublished: statusTimeStamp,
-						flagReason: dojo.byId("cust_input_comment_for_comment").value,
-						flagUserGuid: flagOwnerId,
-						flagUserName: flagOwner,
-						containerId: containerId
-				}),
-				handleAs: "text",
-				// The success handler - Destroys window
-				load: function(response) {
-					setTimeout(function() {eWidgForm.destroyRecursive(); }, 0);
-					//Refreshses stream automatically to display the new content
-					dojo.publish(com.ibm.social.as.constants.events.UPDATESTATE, [{}]);
-				},
-				// The error handler - Destroys window
-				error: function() {
-					setTimeout(function() {eWidgForm.destroyRecursive(); }, 0);
-				},
-				// The complete handler
-				handle: function() {
-				}
-			});
-			setTimeout(function() { eWidgForm.destroyRecursive(); }, 0);
-		});
-	  }
-
-      function iterateXPathResults(iterator) {
+      function iterateXPathResults(nodeList) {
           try {
-              var thisNode = iterator.iterateNext();
+              var thisNode = nodeList.iterateNext();
               while (thisNode) {
                   console.log( thisNode );
-                  thisNode = iterator.iterateNext();
+                  thisNode = nodeList.iterateNext();
               }
           }
           catch (e) {
@@ -239,19 +120,19 @@
 
       }
 
-    function getFeedUrl(faiNode) {
-          var e = document.evaluate( "ancestor::div[contains(@class, 'lotusPost lotusStatus lotusBoard')]", faiNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
-              console.log(e);
-
+    function getCommentId(faiNode) {
+        var lotusPost = document.evaluate( "ancestor::div[contains(@class, 'lotusPost lotusStatus lotusBoard')]", faiNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+        return document.evaluate( "a/@id", lotusPost, null, XPathResult.STRING_TYPE, null ).stringValue.substr(8, 36);
     }
-    function getUrlParameter(url,name) {
-        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-        var results = regex.exec(url);
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-    };
 
-      var hi =function(event) {
+    function getFeedUrl(faiNode) {
+        var feedNode = dojo.query(".lotusFeeds.lotusMeta")[0];
+        //console.log(feedNode);
+        var feedUrl = document.evaluate( "a[@class='lotusAction']/@href", feedNode, null, XPathResult.STRING_TYPE, null ).stringValue;
+        return feedUrl;
+    }
+
+    function customFlagPopup(feedUrl){
            require(
 					[ "dojo/dom-construct", "dojo/dom-style", "dojo/_base/lang", "dojo/on" ],
 					function(domConstruct, domStyle, lang, on) {
@@ -275,7 +156,7 @@
                             setTimeout(function() { eWidgForm.destroyRecursive(); }, 0);
                         });
 
-                        var dataNode = event.target;
+                       // var dataNode = event.target;
                        // var commentDataJSON = getCommentData(event.target);
                        // console.log("### communityUuid=" + commentDataJSON.communityUuid );
                        // console.log("### contentAuthorGuid=" + commentDataJSON.contentAuthorGuid );
@@ -283,9 +164,73 @@
                        // console.log("### contentId=" + commentDataJSON.contentId );
                        // console.log("### contentId=" + commentDataJSON.contentContent );
 
-                        getFeedUrl();
+
 
                     }) //function
+    }
+
+    function resolver(prefix) {
+        switch (prefix) {
+            case 'thr':
+                return 'http://purl.org/syndication/thread/1.0';
+            case 'snx':
+                return 'http://www.ibm.com/xmlns/prod/sn';
+            case 'td':
+                return 'urn:ibm.com/td';
+            case 'opensearch':
+                return 'http://a9.com/-/spec/opensearch/1.1';
+            default:
+                return 'http://www.w3.org/2005/Atom';
+        }
+    }
+
+    function parseXml(xmlDoc, commentId){
+        //console.log(new XMLSerializer().serializeToString(doc.documentElement));
+        //console.log(xmlDoc.documentElement);
+        var entry=document.evaluate("/a:entry[./a:id='urn:lsid:ibm.com:td:"+commentId+"']", xmlDoc.documentElement, resolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        //iterateXPathResults(entry);
+        var contentAuthorGuid = document.evaluate("/a:author/snx:userid/text()", entry, resolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var contentAuthorName = document.evaluate("/a:author/a:name/text()", entry, resolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var contentContent = document.evaluate("/a:content/text()", entry, resolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var statusTimeStamp = document.evaluate("/a:updated/text()", entry, resolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        console.log(entry);
+        console.log(contentAuthorGuid);
+        console.log(contentAuthorName);
+        console.log(contentContent);
+        console.log(statusTimeStamp);
+    }
+
+    function getFeed(feedUrl,commentId) {
+        var xhrArgs = {
+            url: feedUrl,
+            handleAs: "xml",
+            load: function(data){
+                console.log(commentId);
+                console.debug(new XMLSerializer().serializeToString(data.documentElement));
+                var x = parseXml(data,commentId);
+                //console.log(x);
+            },
+            error: function(error){
+
+            }
+        }
+        // Call the asynchronous xhrGet
+        var deferred = dojo.xhrGet(xhrArgs);
+    }
+
+    function getUrlParameter(url,name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(url);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+
+      var faiClicked =function(event) {
+          var commentId = getCommentId(event.target); //event.target is the node of where the FAI link was clicked
+          console.log("commentId=" + commentId);
+          var feedUrl = getFeedUrl(event.target);
+          console.debug("Feed URL=" + feedUrl);
+          getFeed(feedUrl,commentId);
         }
 
 		//Adds FAI link
@@ -300,7 +245,7 @@
                         var li = domConstruct.create("li");
                         var a = domConstruct.create("a", {href: "javascript:", innerHTML: "FLAGGER",title: "Flag as inappropriate", role: "button", className: "flagBtn"}, li);
 						domConstruct.place(li, ul, "last"); //Create a new element and place it last
-                        var handle = on(a, "click", function(event){hi(event);});
+                        var handle = on(a, "click", function(event){faiClicked(event);});
 					});
 
 			// The mutation observer can not be disconnected because on some pages the DOM is updated dynamically so we have to constantly monitor the page
